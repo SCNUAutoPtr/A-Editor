820bf401167a5e30e0dc72fd506ef6fe0b580f8c
“seventh720” <1791303902@qq.com>
2023-04-09 22:28:49 +0800

    搜索栏和评论区已完成

--- src/components/EditorPage.vue
+++ src/components/EditorPage.vue
@@ -1,11 +1,22 @@
 <template>
     <div>
       <button @click="handleClick">跳转到 Show 页面</button>
+
+    <SearchLabel></SearchLabel>
+    <CommentArea></CommentArea>
+
     </div>
   </template>
   
   <script>
+  import SearchLabel from './SearchLabel.vue';
+  import CommentArea from "./CommentArea.vue"
   export default {
+    components:{SearchLabel,CommentArea},
+    data(){
+      return{
+      }
+    },
     methods: {
       handleClick() {
         this.$router.push('/show')
@@ -13,4 +24,3 @@
     }
   }
   </script>
-  
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,19 +2,11 @@
 <div>
   <button @click="handleClick">跳转到 Editor 页面</button>
 
-  <CommentArea></CommentArea>
-
 </div>
 </template>
 
 <script>
-import CommentArea from "./CommentArea.vue"
 export default {
-  components:{CommentArea},
-  data(){
-    return{
-    }
-  },
   methods: {
     handleClick() {
       this.$router.push('/editor')
